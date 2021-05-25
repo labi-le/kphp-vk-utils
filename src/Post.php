c121a716501dbce9328ac03dd04862df2713d533
labile <1a6i1e@gmail.com>
2021-05-25 12:42:36 +0400

    fetch post id from wall post

--- src/Post.php
+++ src/Post.php
@@ -9,7 +9,6 @@ namespace Astaroth\VkUtils;
 use Astaroth\VkUtils\Builders\Builder;
 use Astaroth\VkUtils\Contracts\IPostBuilder;
 use Astaroth\VkUtils\Traits\ParallelProcessingTrait;
-
 use Throwable;
 
 final class Post extends Builder
@@ -22,7 +21,9 @@ final class Post extends Builder
     public function create(IPostBuilder ...$post): array
     {
         $callable = function ($post) {
-            return $this->request('wall.post', get_object_vars($post))['response']['post_id'];
+            $params = get_object_vars($post);
+            $post_id = $this->request('wall.post', $params)['response']['post_id'];
+            return sprintf('wall%s_%s', $params['owner_id'], $post_id);
         };
 
         return $this->isEnabledParallelRequests()
